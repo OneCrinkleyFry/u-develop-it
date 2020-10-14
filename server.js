a0b19bd09e851633736a2149a6c4801f54130880
JoshMiller5994 <talk2joshbgosh@gmail.com>
2020-10-14 11:05:10 -0600

    reorganized my parties routes.

--- server.js
+++ server.js
@@ -7,61 +7,10 @@ const apiRoutes = require('./routes/apiRoutes');
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use('/api', apiRoutes);
-
-app.get('/api/parties', (req, res) => {
-    const sql = `SELECT * FROM parties`;
-    const params = [];
-    db.all(sql, params, (err, rows) => {
-        if (err) {
-            res.status(500).json({ error: err.message });
-            return;
-        }
-
-        res.json({
-            message: 'success',
-            data: rows
-        });
-    });
-});
-
-app.get('/api/party/:id', (req, res) => {
-    const sql = `SELECT * FROM parties WHERE id = ?`;
-    const params = [req.params.id];
-    db.get(sql, paramas, (err, row) => {
-        if (err) {
-            res.status(400).json({ error: err.message });
-            return;
-        }
-
-        res.json({
-            message: 'success',
-            data: row
-        });
-    });
-});
-
-app.delete('/api/party/:id', (req,res) => {
-    const sql = `DELETE FROM parties WHERE id = ?`;
-    const params = [req.params.id];
-    db.run(sql, params, function(err, result) {
-        if (err) {
-            res.status(400).json({ error: res.message });
-            return;
-        }
-
-        res.json({
-            message: 'successfully deleted',
-            changes: this.changes
-        });
-    });
-});
-
-app.use((req, res) => {
-    res.status(404).end();
-});
+app.use((req, res) => { res.status(404).end(); });
 
 db.on('open', () => {
     app.listen(PORT, () => {
-        console.log(`Server running on https://localhost:${PORT}/`);
+        console.log(`Server running on http://localhost:${PORT}/`);
     });
 });
